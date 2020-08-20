8dbf556b053469ea1799bed86ac946057f2e31f3
ChrisWNorton <chriswsnorton@gmail.com>
2020-08-20 12:16:57 -0700

    finish setting up router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import About from './pages/About';
+import Resume from './pages/Resume';
+import Projects from './pages/Projects';
 
 function App() {
   return (
     <div className="App">
-      portfolio
+      <Router>
+        <Route path="/" />
+        <Route path="/about" component={About} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/resume" component={Resume} />
+      </Router>
     </div>
   );
 }
