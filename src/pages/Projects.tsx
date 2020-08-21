794c37c2e0679b027b694df33570ea52900956d4
ChrisWNorton <chriswsnorton@gmail.com>
2020-08-21 16:16:22 -0700

    finish prelim project card

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -13,17 +13,18 @@ const ProjectOuter = styled.div`
 `
 
 const ProjectCard = styled.div`
+    display: flex;
     border-radius: 10px;
     border: 2px solid #fff;
     box-shadow: 0 2px 10px -2px;
     height: 8em;
-    width: 60%;
+    width: 50%;
     background-color: orange;
     cursor: pointer;
     :hover{
         transform: scale(1.1);
     }
-    transition: transform 0.1s ease-in-out;
+    transition: transform 0.2s ease-in-out;
 `
 
 function Projects() {
@@ -31,9 +32,18 @@ function Projects() {
       <Outer>
           <ProjectOuter>
             <ProjectCard>
-                <div>
-                    hi
+                <div style = {{width:'20%'}}>
+                    <img src={require('../assets/GitHubLogo.png')} style={{width:'100%', height:'100%'}}/>
                 </div>
+                <div style= {{padding:'1em', color:'white', width:'100%', textAlign:'left'}}>
+                    <div>
+                        <h3>Passr</h3>
+                    </div>
+                    <div>
+                        An application I worked on as part of a club.
+                    </div>
+                </div>
+                
             </ProjectCard>
           </ProjectOuter>
       </Outer>
