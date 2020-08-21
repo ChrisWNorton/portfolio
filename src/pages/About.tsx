d1b6fd4ab185e0636807789baa3e16eabbdba84d
ChrisWNorton <chriswsnorton@gmail.com>
2020-08-20 20:39:31 -0700

    Add logos to about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -32,10 +32,17 @@ const TextHolder = styled.div`
 const Img = styled.img`
     width: 100%;
     height:20%;
-    @media only screen and (max-width: 768px){
-        height: 60%;
-    }
+`
 
+const LogoHolder = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+const Logo = styled.img`
+    height: 4em;
+    width: 4em;
+    margin: 1em;
 `
 
 function About() {
@@ -58,7 +65,12 @@ function About() {
             development, but I am also very interested in machine learning and the applications of it in
             modern society.
           </Text>
+        <h1 style={{paddingTop:"1em"}}>Skills</h1>
         </TextHolder>
+        <LogoHolder>
+            <Logo src={require("../assets/GitHubLogo.png")}/>
+            <Logo src={require("../assets/LinkedInLogo.png")}/>
+        </LogoHolder>
       </Outer>
     )
 }
