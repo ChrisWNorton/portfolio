265b9183773d9a3f096d1b90c155e213cb1e1900
ChrisWNorton <chriswsnorton@gmail.com>
2020-08-21 15:26:18 -0700

    start building project card

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Outer = styled.div`
+    margin-top: 3.5em;
+`
+
+const ProjectOuter = styled.div`
+    height: 10em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const ProjectCard = styled.div`
+    border-radius: 10px;
+    border: 2px solid #fff;
+    box-shadow: 0 2px 10px -2px;
+    height: 8em;
+    width: 60%;
+    background-color: orange;
+    cursor: pointer;
+    :hover{
+        transform: scale(1.1);
+    }
+    transition: transform 0.1s ease-in-out;
+`
 
 function Projects() {
     return(
-      <div>
-        Projects
-      </div>
+      <Outer>
+          <ProjectOuter>
+            <ProjectCard>
+                <div>
+                    hi
+                </div>
+            </ProjectCard>
+          </ProjectOuter>
+      </Outer>
     )
 }
 
