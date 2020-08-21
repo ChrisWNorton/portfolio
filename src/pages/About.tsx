f9e014b243e92b653f6fddfef92e2c46907e8569
ChrisWNorton <chriswsnorton@gmail.com>
2020-08-20 19:02:56 -0700

    Lint

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -24,22 +24,26 @@ const TextHolder = styled.div`
 
 function About() {
     return(
-    <Outer>
-        <img src={require("../assets/AboutImage.jpg")} style={{width:'100%', height:'40%'}}/>
+      <Outer>
+        <img src={require("../assets/AboutImage.jpg")} style={{ width: '100%', height: '40%' }} />
         <TextHolder>
-            <h1>About Me</h1>
-            <Text>
-            Hi! My name is Christopher Norton, and I was born and raised in Calgary, Alberta, and lived there until I started the Software Engineering program at 
-            the University of Victoria. While growing up, I had several hobbies, including: soccer, mountain biking, swimming and programming simple games. 
-            I played soccer my entire life, and have always enjoyed being part of a team and would describe myself as a team player.
-            </Text>
-            <Text>
-            I have always been very interested in technology, and learned my first programming language (Lua) when I was just 13 years old. This love and curiosity 
-            towards technology has been a trait I continue to have and nurture. Currently, my main interest and area of focus in full stack web development, but I am 
-            also very interested in machine learning and the applications of it in modern society.
-            </Text>
-            </TextHolder>
-    </Outer>
+          <h1>About Me</h1>
+          <Text>
+            Hi! My name is Christopher Norton, and I was born and raised in Calgary, Alberta, and lived there
+            until I started the Software Engineering program at the University of Victoria. While growing up,
+            I had several hobbies, including: soccer, mountain biking, swimming and programming simple games.
+            I played soccer my entire life, and have always enjoyed being part of a team and would describe
+            myself as a team player.
+          </Text>
+          <Text>
+            I have always been very interested in technology, and learned my first programming language (Lua)
+            when I was just 13 years old. This love and curiosity towards technology has been a trait I
+            continue to have and nurture. Currently, my main interest and area of focus in full stack web
+            development, but I am also very interested in machine learning and the applications of it in
+            modern society.
+          </Text>
+        </TextHolder>
+      </Outer>
     )
 }
 
