12cb6d439b546a0dce5cfcf30691e6afa59ad6b7
ChrisWNorton <chriswsnorton@gmail.com>
2020-08-28 19:01:44 -0700

    add title and project image

--- src/molecules/ProjectModal.tsx
+++ src/molecules/ProjectModal.tsx
@@ -1,18 +1,37 @@
 import React from "react";
 import styled from "styled-components";
-import { Modal } from "react-bootstrap";
+import { Modal, Button } from "react-bootstrap";
 
 interface props {
   show: boolean;
   onHide: () => void;
 }
-function ProjectModal(props: props) {
+
+const InfoHolder = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+function ProjectModal({ show, onHide }: props) {
   return (
-    <Modal {...props} centered>
+    <Modal show={show} onHide={onHide} centered size="lg">
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">Passr</Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">
+          <h1>Passr</h1>
+        </Modal.Title>
       </Modal.Header>
-      <Modal.Body>hello</Modal.Body>
+      <Modal.Body>
+        <InfoHolder>
+          <img
+            src={require("../assets/GestureRecognition.png")}
+            style={{ width: "100%", padding: "2em" }}
+            alt="Poster"
+          />
+        </InfoHolder>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={onHide}>Close</Button>
+      </Modal.Footer>
     </Modal>
   );
 }
